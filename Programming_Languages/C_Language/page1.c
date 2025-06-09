17857f95a9f7122aef8bef5c3d20ed4a8d1c53c0
kapil-bhargava <kapilkkb28@gmail.com>
2025-06-09 12:33:20 +0530

    add more file of js

--- Programming_Languages/C_Language/page1.c
+++ Programming_Languages/C_Language/page1.c
@@ -1,5 +1,15 @@
 #include <stdio.h>
 
+
+void start(){
+    char name[];
+    printf("Enter your name: \n");
+    scanf("%s", name);
+    printf("Hello, %s!\n", name);
+    for(int i = 0; i < 5; i++) {
+        printf("This is line %d\n", i + 1);
+    }
+}
 int main() {
     printf("Hello, World in C!\n");
     return 0;
