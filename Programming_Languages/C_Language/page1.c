0ba10a17961557e49f5fdcd2b8d7b84c34c374b1
kapil-bhargava <kapilkkb28@gmail.com>
2025-06-13 13:03:07 +0530

    react changes file and genai

--- Programming_Languages/C_Language/page1.c
+++ Programming_Languages/C_Language/page1.c
@@ -1,16 +1,77 @@
+// Compile with: cl volume_control_input.c /link Ole32.lib
+
+#include <windows.h>
+#include <mmdeviceapi.h>
+#include <endpointvolume.h>
 #include <stdio.h>
 
-
-void start(){
-    char name[];
-    printf("Enter your name: \n");
-    scanf("%s", name);
-    printf("Hello, %s!\n", name);
-    for(int i = 0; i < 5; i++) {
-        printf("This is line %d\n", i + 1);
-    }
-}
 int main() {
-    printf("Hello, World in C!\n");
+    HRESULT hr;
+    CoInitialize(NULL); // Initialize COM
+
+    IMMDeviceEnumerator* deviceEnumerator = NULL;
+    IMMDevice* defaultDevice = NULL;
+    IAudioEndpointVolume* endpointVolume = NULL;
+
+    int volumePercent = 0;
+
+    printf("Enter desired system volume (0 to 100): ");
+    scanf("%d", &volumePercent);
+
+    if(volumePercent < 0 || volumePercent > 100) {
+        printf("Invalid volume! Please enter a value between 0 and 100.\n");
+        return -1;
+    }
+
+    // Convert to float scale (0.0 to 1.0)
+    float newVolume = volumePercent / 100.0f;
+
+    // Create device enumerator
+    hr = CoCreateInstance(
+        &CLSID_MMDeviceEnumerator, NULL, CLSCTX_INPROC_SERVER,
+        &IID_IMMDeviceEnumerator, (LPVOID*)&deviceEnumerator);
+
+    if (FAILED(hr)) {
+        printf("Failed to create device enumerator.\n");
+        return -1;
+    }
+
+    // Get default audio endpoint (output device)
+    hr = deviceEnumerator->lpVtbl->GetDefaultAudioEndpoint(
+        deviceEnumerator, eRender, eConsole, &defaultDevice);
+
+    if (FAILED(hr)) {
+        printf("Failed to get default audio endpoint.\n");
+        deviceEnumerator->lpVtbl->Release(deviceEnumerator);
+        return -1;
+    }
+
+    // Activate the endpoint volume interface
+    hr = defaultDevice->lpVtbl->Activate(
+        defaultDevice, &IID_IAudioEndpointVolume,
+        CLSCTX_INPROC_SERVER, NULL, (LPVOID*)&endpointVolume);
+
+    if (FAILED(hr)) {
+        printf("Failed to activate endpoint volume interface.\n");
+        defaultDevice->lpVtbl->Release(defaultDevice);
+        deviceEnumerator->lpVtbl->Release(deviceEnumerator);
+        return -1;
+    }
+
+    // Set master volume
+    hr = endpointVolume->lpVtbl->SetMasterVolumeLevelScalar(endpointVolume, newVolume, NULL);
+
+    if (SUCCEEDED(hr)) {
+        printf("Volume set to %d%% successfully.\n", volumePercent);
+    } else {
+        printf("Failed to set volume.\n");
+    }
+
+    // Clean up
+    endpointVolume->lpVtbl->Release(endpointVolume);
+    defaultDevice->lpVtbl->Release(defaultDevice);
+    deviceEnumerator->lpVtbl->Release(deviceEnumerator);
+    CoUninitialize();
+
     return 0;
 }
